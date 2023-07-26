5d790a595fdb33ed09a74832b5913e58394ae889
AAWorks <axalonso12@gmail.com>
2023-07-26 17:38:44 +0000

    storage mli done

--- lib/storage.mli
+++ lib/storage.mli
@@ -2,3 +2,17 @@ open! Core
 open! Scrape
 open! Async
 
+module House : sig
+    type t
+
+    val string_price : t -> string
+    val address : t -> string
+    val specs : t -> string
+
+    val fields : string list
+
+    val from_scraped_data : mapped_details:((string, string, 'a) Map_intf.Map.t) -> images:(string list) -> t 
+
+  end
+
+val pull_data : unit -> unit Deferred.t
